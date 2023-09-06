630f6fe0fa6a7298ebc64b68418d565602652853
kyle.hou <kyle.hou@meetsocial.com>
2023-09-06 22:47:58 +0800

    first commit

---
+++ base-redis/src/main/java/com/openkeji/redis/BaseRedisApplication.java
@@ -0,0 +1,13 @@
+package com.openkeji.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseRedisApplication.class, args);
+	}
+
+}
